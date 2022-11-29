910047e3a8001236593aaaec4bc0f882a94f9954
claireguyatt <cg5782@gmail.com>
2022-11-28 20:03:21 -0500

    changes for deployment

--- ht/settings.py
+++ ht/settings.py
@@ -155,3 +155,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://ht-production.up.railway.app/'
+]
