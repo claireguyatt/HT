054ed4769dd49a5f574cdf45a483fbd019831780
claireguyatt <cg5782@gmail.com>
2023-01-16 18:14:47 -0500

    turned debug off for production

--- ht/settings.py
+++ ht/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uirs(-&c@)3b%0pl0k=r#h#*0skjtldg#rrjs7$a)3b26&$!_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
