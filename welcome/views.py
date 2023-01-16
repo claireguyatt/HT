06e0a609ea95e5c1cb668cfebd8f58d6bb6aa0b7
claireguyatt <cg5782@gmail.com>
2023-01-16 18:09:34 -0500

    turned debug off for production & added guest login button

--- welcome/views.py
+++ welcome/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib import messages
+from django.contrib.auth.models import UserManager
 
 from .forms import NewUserForm
 from .models import Profile
@@ -36,3 +37,9 @@ def register(request):
         print("Unsuccessful registration. Invalid information.")
         
     return render(request, 'registration/register.html', {'form': NewUserForm})
+
+def explore(request):
+
+    guest_user = UserManager.create_user(username="guest", email=None, password=None)
+    login(request, guest_user)
+    return redirect ('homepage')
