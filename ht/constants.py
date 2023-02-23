ffc577136686012881f8fee3823e2d0ff4a65d17
claireguyatt <cg5782@gmail.com>
2023-02-23 12:34:15 -0500

    hidding database

--- ht/constants.py
+++ ht/constants.py
@@ -1,10 +1,10 @@
-DATABASE = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'lzNip2fw2SbwwFqMfAAY',
-        'HOST': 'containers-us-west-136.railway.app',
-        'PORT': 6317
+        'NAME': 'HT',
+        'USER': 'htAdmin',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
